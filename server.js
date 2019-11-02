21858fb7d9410bddbdd343832e4028d310dfa5cb
Ruslan Oliinyk <russ.oliinyk@gmail.com>
2019-11-02 18:54:06 +0200

    Hotfix

--- server.js
+++ server.js
@@ -2,10 +2,20 @@ const express        = require('express');
 const MongoClient    = require('mongodb').MongoClient;
 const bodyParser     = require('body-parser');
 const app            = express();
-const port           = 3000;
+const port           = process.env.PORT || 5000;
 
 require('dotenv').config();
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(function(request, response, next){
+    let now = new Date();
+    let hour = now.getHours();
+    let minutes = now.getMinutes();
+    let seconds = now.getSeconds();
+    let data = `${hour}:${minutes}:${seconds} ${request.method} ${request.url} ${request.get("user-agent")}`;
+    console.log(data);
+    next();
+});
 
 MongoClient.connect(process.env.MONGOLAB_OLIVE_URI, { useUnifiedTopology: true }, (err, client) => {
   if (err) return console.log(err)
@@ -14,6 +24,6 @@ MongoClient.connect(process.env.MONGOLAB_OLIVE_URI, { useUnifiedTopology: true }
   require('./app/routes')(app, database);
 
   app.listen(port, () => {
-    console.log('We are live on: http://localhost:' + port);
+    console.log('We are live on:' + port);
   });
 });
