286d972aa2f523a0532bf452b6df4478824a2fd1
Ruslan Oliinyk <russ.oliinyk@gmail.com>
2019-11-02 21:02:21 +0200

    Added new fields to user

--- routes/user_routes.js
+++ routes/user_routes.js
@@ -31,7 +31,13 @@ module.exports = function(app, db) {
   app.post('/users', (req, res) => {
     const user = {
       name: req.body.name,
-      macAddress: req.body.macAddress
+      station_mac: req.body.station_mac,
+      first_time_seen: req.body.first_time_seen,
+      last_time_seen: req.body.last_time_seen,
+      power: req.body.power,
+      packets: req.body.packets,
+      bssid: req.body.bssid,
+      essids: req.body.essids
     };
 
     db.collection('users').insertOne(user, (err, result) => {
@@ -64,7 +70,7 @@ module.exports = function(app, db) {
     const id = req.params.id;
     const details = { '_id': new ObjectID(id) };
 
-    db.collection('users').remove(details, (err, item) => {
+    db.collection('users').deleteOne(details, (err, item) => {
       if (err) {
         res.send(err);
       } else {
