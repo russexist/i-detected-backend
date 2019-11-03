95d8e3b4c5bfac380a350c10a2f019745ace40c8
Ihor Levchenko <ifree92@gmail.com>
2019-11-03 07:29:31 +0200

    feats

--- services/users-updater.js
+++ services/users-updater.js
@@ -1,34 +1,23 @@
 const WifiState = require("../services/WifiState");
+const UsersManager = require("./UsersManager");
 
 function init(db) {
   WifiState.on("data", list => {
-    const macAddresses = list.map((elem, index) => {
-      return elem.station_mac.toUpperCase();
-    });
+    const curTime = Date.now();
 
-    const newData = list.map(item => {
-      const since = new Date(item.last_time_seen + "Z");
-      const newItem = {
-        mac: item.station_mac,
-        signal: item.power,
-        since: Date.now() - since.getTime()
-      };
-      return newItem;
-    });
-
-    newData
-      .sort((a, b) => (a.signal < b.signal ? 1 : -1))
-      .filter(item => item.signal >= -50 && item.since <= 30000)
-      .forEach(item => {
-        db.collection("users").findOne(
-          { station_mac: item.mac },
-          (err, result) => {
-            let user = { ...{ name: result.name }, ...item };
-            io.emit("data-list", user);
-            // console.log(user);
-          }
-        );
+    const newData = list
+      .map(item => {
+        const user = UsersManager.getUserByMac(item.station_mac);
+        return { ...item, name: user ? user.name : "" };
+      })
+      .filter(item => {
+        const since = new Date(item.last_time_seen + "z");
+        return !!(curTime - since <= 30000 && item.name);
       });
+
+    // .forEach(item => console.log(item));
+    console.log(newData);
+    io.emit("data-list", newData);
   });
 }
 
