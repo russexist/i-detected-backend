4392129aaa6956270bdf307b53b695e53e5b8093
Ruslan Oliinyk <russ.oliinyk@gmail.com>
2019-11-03 06:09:29 +0200

    added user sorting

--- services/users-updater.js
+++ services/users-updater.js
@@ -6,13 +6,21 @@ function init(db) {
       return elem.station_mac.toUpperCase();
     });
 
-    // { station_mac: { $in: macAddresses }, $and: [{ power:  { $ne: [0, -1, '', '0', '-1'] } }] }
-    db.collection("users")
-      .find({ station_mac: { $in: macAddresses } })
-      .sort({ power: -1 })
-      .toArray((err, users) => {
-        io.emit("data-list", users);
-        // console.log(users);
+    const newData = list.map((item) => {
+      const since = new Date(item.last_time_seen + 'Z');
+      const newItem = { mac: item.station_mac, signal: item.power, since: Date.now() - since.getTime() };
+      return newItem;
+    });
+
+    newData
+      .sort((a, b) => (a.signal < b.signal ? 1 : -1))
+      .filter((item) => item.signal >= -50 && item.since <= 30000)
+      .forEach((item) => {
+        db.collection("users").findOne({ station_mac: item.mac }, (err, result) => {
+          let user = { ...{ name: result.name }, ...item };
+          io.emit("data-list", user);
+          console.log(user);
+        })
       });
   });
 }
