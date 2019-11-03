cfa7496d038aa8d3295c2b487febdbd44f421900
Ruslan Oliinyk <russ.oliinyk@gmail.com>
2019-11-03 12:10:55 +0200

    Fixed PUT user endpoint

--- routes/user_routes.js
+++ routes/user_routes.js
@@ -84,12 +84,6 @@ module.exports = function(app, db) {
     const user = {
       name: req.body.name,
       station_mac: req.body.station_mac,
-      first_time_seen: req.body.first_time_seen,
-      last_time_seen: req.body.last_time_seen,
-      power: req.body.power,
-      packets: req.body.packets,
-      bssid: req.body.bssid,
-      essids: req.body.essids,
       text: req.body.text,
       text_color: req.body.text_color
     };
